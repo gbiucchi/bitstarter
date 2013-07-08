53bbe5c9c735d9dc4e86c3823bd9e7194511822f
gbiucchi <biucchi@freenet.de>
2013-07-08 12:24:17 +0200

    Updated web.js: using index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  string = fs.readFileSync("index.html").toString("utf-8");
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
