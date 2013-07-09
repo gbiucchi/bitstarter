0660f7e07170e4ed12d6037a7417952fb6cabd8f
gbiucchi <biucchi@freenet.de>
2013-07-09 13:59:30 +0200

    Updated grader.js

--- grader.js
+++ grader.js
@@ -19,6 +19,10 @@ References:
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+
+ + restler
+   - https://github.com/danwrong/restler
+
 */
 
 var fs = require('fs');
@@ -26,6 +30,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var URL_DEFAULT = "nothing"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +41,11 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var returnURL = function(infile) {
+    var instr = infile.toString();
+    return instr;
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -61,14 +72,47 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
+var buildCheck = function(checksfile) {
+    var checkURL = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + response);
+        } else {
+            // console.log(result);
+
+            // var checkJson = checkHtmlFile(program.file, program.checks);
+            $ = cheerio.load(result);
+            var checks = loadChecks(checksfile).sort();
+            var out = {};
+            for(var ii in checks) {
+                var present = $(checks[ii]).length > 0;
+                out[checks[ii]] = present;
+            }
+            checkJson = out;
+
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        }
+    };
+    return checkURL;
+};
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_path>', 'URL to download', clone(returnURL), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if( program.url != "nothing" ){
+        var checkURL = buildCheck(program.checks);
+        rest.get(program.url).on('complete', checkURL);
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
